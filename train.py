4722ea8fe8ac5349a75962c48d383821c9d6f825
Contat <support@contat.eu>
2020-02-19 11:37:06 +0100

    updates

--- train.py
+++ train.py
@@ -18,28 +18,29 @@ TRAIN_DIR = 'train'
 TEST_DIR = 'test'
 MODEL_DIR = 'model'
 LOGS_DIR = 'logs'
-BATCH_SIZE = 39
-EPOCHS = 10
+
+TRAIN_COUNT = len(os.listdir(TRAIN_DIR))
+TEST_COUNT = len(os.listdir(TEST_DIR))
+
+BATCH_SIZE = TRAIN_COUNT
+EPOCHS = 100
 IMG_HEIGHT = 150
 IMG_WIDTH = 150
 DEBUG = True
 
 # %% DATAGEN SETUP
 
-total_train = len(os.listdir(TRAIN_DIR))
-total_test = len(os.listdir(TEST_DIR))
-
 train_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_data_gen = train_datagen.flow_from_directory(
     TRAIN_DIR, target_size=(
-        IMG_WIDTH, IMG_WIDTH), batch_size=BATCH_SIZE, class_mode='binary'
+        IMG_WIDTH, IMG_WIDTH), batch_size=BATCH_SIZE, class_mode='categorical'
 )
 
 test_data_gen = test_datagen.flow_from_directory(
     TEST_DIR, target_size=(
-        IMG_WIDTH, IMG_WIDTH), batch_size=BATCH_SIZE, class_mode='binary'
+        IMG_WIDTH, IMG_WIDTH), batch_size=BATCH_SIZE, class_mode='categorical'
 )
 # %% CREATE MODEL
 
@@ -53,10 +54,10 @@ model = Sequential([
     MaxPooling2D(),
     Flatten(),
     Dense(512, activation='relu'),
-    Dense(1)
+    Dense(TRAIN_COUNT, activation='softmax')
 ])
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # view all the layers of the network using the model
 model.summary()
@@ -65,10 +66,10 @@ model.summary()
 
 history = model.fit_generator(
     train_data_gen,
-    steps_per_epoch=total_train // BATCH_SIZE,
+    steps_per_epoch=TRAIN_COUNT // BATCH_SIZE,
     epochs=EPOCHS,
     validation_data=test_data_gen,
-    validation_steps=total_test // BATCH_SIZE
+    validation_steps=TEST_COUNT // BATCH_SIZE
 )
 
 # %% DISPLAY TRAING GRAPH
